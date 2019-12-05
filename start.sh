f6e4f80708190bc64009e1294619f9652da8f9ae
arash <arash99s@yahoo.com>
2019-12-05 10:02:26 -0500

    add initialize function

--- start.sh
+++ start.sh
@@ -9,35 +9,41 @@
 
 # initializing
 declare -i n
-read -p "enter number of games running simultaneously: " n
-read -t 5 -p "enter tag: " tag
-if [[ $tag = "" ]]
-then
-  echo
-fi
-
 declare -i ssp
-read -t 5 -p "enter servers start port: " ssp
-if [ $ssp -eq 0 ]
-then
-  ssp=6000
-  echo "ssp=6000"
-fi
-
 declare -i spd
-read -t 5 -p "enter servers port difference: " spd
-if [ $spd -eq 0 ]
-then
-  spd=10
-  echo "spd=10"
-fi
+tag=""
+
+initialize(){
+  read -p "enter number of games running simultaneously: " n
+  ############
+  read -t 5 -p "enter tag: " tag
+  if [[ $tag = "" ]]
+  then
+    echo
+  fi
+  ############
+  read -t 5 -p "enter servers start port: " ssp
+  if [ $ssp -eq 0 ]
+  then
+    ssp=6000
+    echo "ssp=6000"
+  fi
+  ###########
+  read -t 5 -p "enter servers port difference: " spd
+  if [ $spd -eq 0 ]
+  then
+    spd=10
+    echo "spd=10"
+  fi
+}
+
+initialize
 
 DIR=`dirname $0`
 echo "start" $$ >> proc.txt
 # running runOnPort script for each set of games (n times)
 declare -i i
 declare -i port=$ssp
-declare -i lim=$n-1
 for (( i= 0; i<n; i++))
 do
   $DIR/runOnPort.sh $port $n $i $tag &
