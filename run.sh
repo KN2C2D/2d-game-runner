933e9f20d2a2375ee06782f42958b08b3840a44f
arash <arash99s@yahoo.com>
2019-12-05 11:17:08 -0500

    add function to run.sh

--- run.sh
+++ run.sh
@@ -5,48 +5,53 @@
 #$3 ---> port
 #$4 ---> tag
 
+#############################variables
 DIR=`dirname $0`
-
 declare -i port=$3
 declare -i coach_port=$3+1
 declare -i olcoach_port=$3+2
 tag=$4
 Team1=$1
 Team2=$2
-
+#finding date for adding to log files
+D="$(date +%Y%m%d%H%M%S)"
 tmpDirName="$Team1-$Team2"
+result1=""
+result2=""
+##############################
+
 if ! [ -d $DIR/$tmpDirName ]
 then
   mkdir $tmpDirName
 fi
 cd $DIR/$tmpDirName
 
-#running Games
-../teams/$Team1/startAll $port &
-../teams/$Team2/startAll $port &
-rcssserver server::synch_mode=true server::verbose=off server::port=$port \
-server::coach_port=$coach_port server::olcoach_port=$olcoach_port &
-echo "server $port $!" >> ../proc.txt 
-wait
+runServerAndAgents(){
+  ../teams/$Team1/startAll $port &
+  ../teams/$Team2/startAll $port &
+  rcssserver server::synch_mode=true server::verbose=off server::port=$port \
+  server::coach_port=$coach_port server::olcoach_port=$olcoach_port &
+  echo "server $port $!" >> ../proc.txt 
+  wait
+}
+findResults(){
+  #extracting results from log files to adding to new log files
+  declare -i i=0
+  tmp=`ls *.rcg`
+  i=`expr index $tmp "_"`
+  tmp=${tmp:$i}
+  i=`expr index $tmp "-vs"`
+  result1=${tmp:0:i-1}
+  tmp=${tmp:i+3}
+  i=`expr index $tmp "_"`
+  tmp=${tmp:i}
+  i=`expr index $tmp "."`
+  result2=${tmp:0:i-1}
+}
 
-
-#finding date for adding to log files
-D="$(date +%Y%m%d%H%M%S)"
-
-#extracting results from log files to adding to new log files
-declare -i i=0
-tmp=`ls *.rcg`
-i=`expr index $tmp "_"`
-tmp=${tmp:$i}
-i=`expr index $tmp "-vs"`
-rt1=${tmp:0:i-1}
-tmp=${tmp:i+3}
-i=`expr index $tmp "_"`
-tmp=${tmp:i}
-i=`expr index $tmp "."`
-rt2=${tmp:0:i-1}
-
-logName="$Team1-$rt1-vs-$Team2-$rt2-$D"
+runServerAndAgents
+findResults
+logName="$Team1-$result1-vs-$Team2-$result2-$D"
 
 mv ./*.rcg "$logName.rcg"
 mv ./*.rcl "$logName.rcl"
@@ -58,17 +63,18 @@ then
   mkdir $DIR/results
 fi
 
-#tagging
-if [ -n $tag ]
-then
-  if ! [ -d $DIR/results/$tag ]
+moveResultsToTag(){
+  if [ -n $tag ]
   then
-    mkdir $DIR/results/$tag
+    if ! [ -d $DIR/results/$tag ]
+    then
+      mkdir $DIR/results/$tag
+    fi
+    mv $DIR/$tmpDirName/$logName.rc? $DIR/results/$tag/
+  else
+    mv $DIR/$tmpDirName/$logName.rc? $DIR/results/
   fi
+}
 
-  mv $DIR/$tmpDirName/$logName.rc? $DIR/results/$tag/
-else
-  mv $DIR/$tmpDirName/$logName.rc? $DIR/results/
-fi
-
+moveResultsToTag
 rm -rf $DIR/$tmpDirName
