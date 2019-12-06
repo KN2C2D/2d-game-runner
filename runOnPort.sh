1f750c03c365412e60d98ca561d8e0b5f9c28d09
arash <arash99s@yahoo.com>
2019-12-06 09:54:58 -0500

    better functional

--- runOnPort.sh
+++ runOnPort.sh
@@ -1,50 +1,45 @@
 #! /bin/bash
 
 # run run script for k * n + m games from Games.txt on specifiedn port
+#######################################input
 # $1 -> Port for the games to be run on
 # $2 -> n
 # $3 -> m
 # $4 -> global_tag (optional)
-
+######################################variables
 declare -i port=$1
 declare -i i=0
 declare -i n=$2
 declare -i m=$3
 global_tag=$4
-
-
-#$$ : the process number of the current shell
-echo "runOnPort" $port $$ >> proc.txt
-
-DIR=`dirname $0`
-cd $DIR
-input="Games.txt"
-
 declare -i idx=0
 declare -i flag
-
-while IFS= read -r line
+#########################################methods
+initialize_Arr(){
+  for word in $line
+    do
+      Arr[$idx]=$word
+      idx=$idx+1
+    done
+}
+readFileAndRun(){
+  while IFS= read -r line
 do
   if [ $i -eq $m ]
   then
     idx=0
-    for word in $line
-    do
-      A[$idx]=$word
-      idx=$idx+1
-    done
-
+    initialize_Arr
     if [ $idx -eq 2 ]
     then
-      t1=${A[0]}
-      t2=${A[1]}
+      t1=${Arr[0]}
+      t2=${Arr[1]}
       tag=""
       flag=0
     elif [ $idx -eq 3 ]
     then
-      t1=${A[0]}
-      t2=${A[1]}
-      tag=${A[2]}
+      t1=${Arr[0]}
+      t2=${Arr[1]}
+      tag=${Arr[2]}
       flag=0
     else
       flag=1
@@ -62,3 +57,15 @@ do
     i=0
   fi
 done < $input
+}
+
+#########################################
+
+#$$ : the process number of the current shell
+echo "runOnPort" $port $$ >> proc.txt
+
+DIR=`dirname $0`
+cd $DIR
+input="Games.txt"
+
+readFileAndRun
