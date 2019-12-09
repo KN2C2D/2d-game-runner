fa64b24c70d420683cc09e063a61ae3c3b401982
Amir Mirzaei <amir.mirzaei1379@gmail.com>
2019-12-09 16:47:16 +0330

    - update script updated -> temp path can now be specified in remoteAddresses.txt after remote address

--- updateRemote.sh
+++ updateRemote.sh
@@ -5,21 +5,38 @@ input="$DIR/remoteAddresses.txt"
 
 DEFAULT_TMP_PATH="Desktop/tmpNetworkTest"
 
-declare -i idx
+declare -i count
+declare -i runFlag
 
 while IFS= read -r line
 do
-  if [ -n $line ]
+  count=0
+  for word in $line
+  do
+    W[count]=$word
+    count+=1
+  done
+
+  if [ $count -eq 2 ]
   then
-    idx=`expr index $line '@'`
-    user=${line:0:idx-1}
-    P="/home/$user/$DEFAULT_TMP_PATH"
-    ssh $line mkdir -p $P &
-    scp -rC $DIR/[!.]* "$line:$P" >"/tmp/tmpSCPLOG.txt" &
-    echo $line
-    echo $user
-    echo $P
+    server=${W[0]}
+    tmpPath=${W[1]}
+  elif [ $count -eq 1 ]
+  then
+    server=${W[0]}
+    declare -i idx=`expr index ${W[0]} '@'`
+    user=${W[0]:0:idx-1}
+    tmpPath="/home/$user/$DEFAULT_TMP_PATH"
   fi
+
+  if ! [ $count -eq 0 ]
+  then
+    ssh $server mkdir -p $tmpPath &
+    sleep 0.05
+    scp -rC $DIR/[!.]* "$server:$tmpPath" >/tmp/tmp_scp_log.txt &
+  fi
+
+  echo $line
 done < $input
 
 wait
