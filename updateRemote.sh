f75c8bf2e3e74621ba9e6a19ac82ca7c94f832c1
Amir Mirzaei <amir.mirzaei1379@gmail.com>
2019-12-09 16:05:14 +0330

    update script created

---
+++ updateRemote.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+DIR=`dirname $0`
+input="$DIR/remoteAddresses.txt"
+
+DEFAULT_TMP_PATH="Desktop/tmpNetworkTest"
+
+declare -i idx
+
+while IFS= read -r line
+do
+  if [ -n $line ]
+  then
+    idx=`expr index $line '@'`
+    user=${line:0:idx-1}
+    P="/home/$user/$TMP_PATH"
+    ssh $line mkdir -p $P &
+    scp -rC $DIR/[!.]* "$line:$P" &
+    echo $line
+    echo $user
+    echo $P
+  fi
+done < $input
+
+wait
